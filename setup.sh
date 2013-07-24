9a71113ded8547020a867d64cac2040eed7f235c
kazunobu <kazunobu@MacBookPro.local>
2013-07-24 14:41:15 +0900

    セットアップスクリプトを追加

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+[[ -e ~/dotfiles ]] || git clone git@github.com:bobchin/dotfiles.git ~/dotfiles
+pushd ~/dotfiles
+
+git submodule init
+git submodule update
+
+for i in `ls -a`
+do
+  [ $i = "." ] && continue
+  [ $i = ".." ] && continue
+  [ $i = ".git" ] && continue
+  [ $i = "README.md" ] && continue
+  [ $i = "setup.sh" ] && continue
+  ln -s ~/dotfiles/$i ~/
+done
+
+#vim -c ':NeoBundleInstall' -c ':q!' -c ':q!'
+#if [ `uname` = "Darwin" ]; then
+#  pushd ~/dotfiles/.vim/bundle/vimproc
+#  make -f make_mac.mak
+#  popd
+#fi
+
+popd
